cd540031eaad99464521dcae209c389ccdfc95c7
Rizal Febrianto <r.febriantofficial@gmail.com>
2019-07-22 20:19:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ var carA = document.getElementById('carA');
 var carB = document.getElementById('carB');
 var container = document.getElementById('container');
 
-var carALeft = 400;
-var carBLeft = 400;
+var carALeft = 10;
+var carBLeft = 10;
 
 // Sistem Balapan
 
@@ -12,24 +12,18 @@ function anim(e) {
 if (e.keyCode === 39) {
     carALeft += 5;
     carA.style.left = carALeft + 'px';
-    if (carALeft >= 845)
-    alert('Pemenangnya Car 1')
+    if (carALeft >= 465) {
+        alert('Pemenangnya Car 1')
     }
+  }
 
 if (e.keyCode === 37) {
     carBLeft += 5;
     carB.style.left = carBLeft + 'px';
-    if (carBLeft >= 845)
-    alert('Pemenangnya Car 2')
+    if (carBLeft >= 465) {
+        alert('Pemenangnya Car 2')
     }
+  }
 }
 
 document.onkeydown = anim;
-
-// Config button
-
-// var button = document.getElementById('button');
-
-// function butto(n) {
-//     if 
-// }
