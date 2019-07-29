31dc15bbc98406db3cdcc865314e840aa4dd9e19
Rizal Febrianto <r.febriantofficial@gmail.com>
2019-07-29 18:15:09 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -12,26 +12,18 @@ function anim(e) {
 if (e.keyCode === 39) {
     carALeft += 5;
     carA.style.left = carALeft + 'px';
-    if (carALeft >= 465) {
-        alert('Pemenangnya Car 1')
+    if (carALeft >= 460) {
+        alert('Pemenangnya Car 1');
     }
   }
 
 if (e.keyCode === 37) {
     carBLeft += 5;
     carB.style.left = carBLeft + 'px';
-    if (carBLeft >= 465) {
-        alert('Pemenangnya Car 2')
+    if (carBLeft >= 460) {
+        alert('Pemenangnya Car 2');
     }
   }
 }
 
 document.onkeydown = anim;
-
-// Config button
-
-// var button = document.getElementById('button');
-
-// function butto(n) {
-//     if 
-// }
